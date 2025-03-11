d6b73a14282216246093036e6954371be540a111
badevil666 <ctom71718@gmail.com>
2025-03-11 14:09:42 +0530

    things are moving

--- Backend/Routes/User/getDashboard.js
+++ Backend/Routes/User/getDashboard.js
@@ -33,9 +33,31 @@ const getDashboard = async (req, res) =>
        let response = {
         disastersInTaluk : null,
         disasterNearby : null,
-        reports : null
+        reportInTaluk : null,
+        reportsNearby : null
        } 
-       const {aadhar} = req.body
+        let talukReportQuery = 'SELECT R.* FROM Report R JOIN Users U ON U.districttalukid = R.taluk WHERE U.Aadhar = $1'
+       let nearbyReportQuery = `SELECT 
+       D.*,
+       ( 
+           6371 * acos(
+               cos(radians(U.CurrentLocation[1])) * cos(radians(D.occurredLocation[1])) * 
+               cos(radians(D.occurredLocation[0]) - radians(U.CurrentLocation[0])) + 
+               sin(radians(U.CurrentLocation[1])) * sin(radians(D.occurredLocation[1]))
+           )
+       ) AS distance_km
+   FROM Disaster D
+   JOIN Users U ON U.Aadhar = $1
+   WHERE 
+       ( 
+           6371 * acos(
+               cos(radians(U.CurrentLocation[1])) * cos(radians(D.occurredLocation[1])) * 
+               cos(radians(D.occurredLocation[0]) - radians(U.CurrentLocation[0])) + 
+               sin(radians(U.CurrentLocation[1])) * sin(radians(D.occurredLocation[1]))
+           )
+       ) <= 5
+   ORDER BY distance_km` 
+        const {aadhar} = req.body
        console.log(req.body)
        let nearByDisasters = await dbClient.query('select getnearbydisaster($1)', [aadhar])
        console.log(nearByDisasters.rows)
@@ -44,8 +66,12 @@ const getDashboard = async (req, res) =>
        console.log(talukDisaster.rows)
        console.log(response)
        response.disastersInTaluk = parseDisasterData(talukDisaster.rows)
+       let talukReport = await dbClient.query(talukReportQuery, [aadhar])
 
+       response.reportInTaluk = talukReport.rows
        
+       let nearbyReport = await dbClient.query(nearbyReportQuery, [aadhar])
+       response.reportsNearby = nearbyReport.rows
        console.log(response.disasterNearby)
        console.log(response)
 
