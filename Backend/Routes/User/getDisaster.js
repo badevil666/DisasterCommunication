2d00f0e9c24da425e1aa98dea3371dfe6fe15b57
badevil666 <ctom71718@gmail.com>
2025-03-11 20:52:32 +0530

    things are moving

--- Backend/Routes/User/getDisaster.js
+++ Backend/Routes/User/getDisaster.js
@@ -6,7 +6,7 @@ const getDisaster = async (req, res) => {
   let response = {data : null}
   console.log(req.body)
   const { disasterID, aadhar } = req.body
-  response.data = (await dbClient.query('select d.*, CASE WHEN dv.aadhar is not null then true else false end as volunteered from disaster as d left join disastervolunteer as dv on d.id = dv.disasterid and dv.aadhar = $2 where d.id = $1 ', [disasterID, aadhar])).rows
+  response.data = (await dbClient.query('select d.*, r.disastertype , CASE WHEN dv.aadhar is not null then true else false end as volunteered from disaster as d left join disastervolunteer as dv on d.id = dv.disasterid and dv.aadhar = $2 join report r on r.id = d.reportid where d.id = $1 ', [disasterID, aadhar])).rows
   let videoPath = (await dbClient.query('select r.video from disaster as d join report as r on d.reportid=r.id where d.id = $1', [disasterID]))
   console.log(videoPath)
   console.log(response)
